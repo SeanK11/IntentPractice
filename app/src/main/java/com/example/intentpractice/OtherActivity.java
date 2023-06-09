1d43c198c6cd267901b51ebdb8c2c8b550f7362d
Sunghyun Kim <wkddls423@gmail.com>
2023-06-10 02:46:08 +0900

    finish() 이용 => 현재화면 종료, 이전화면 복귀

--- app/src/main/java/com/example/intentpractice/OtherActivity.java
+++ app/src/main/java/com/example/intentpractice/OtherActivity.java
@@ -2,13 +2,27 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class OtherActivity extends AppCompatActivity {
+ public class OtherActivity extends AppCompatActivity {
+    private Button returnToMainBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+        returnToMainBtn = findViewById(R.id.returnToMainBtn);
+        returnToMainBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent myIntent = new Intent(OtherActivity.this, MainActivity.class);
+//                startActivity(myIntent);
+                finish();
+            }
+        });
     }
 }
