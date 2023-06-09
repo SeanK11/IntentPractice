e83e9d8ae1d39673b23b559dd9adfd17c3dcccca
Sunghyun Kim <wkddls423@gmail.com>
2023-06-10 01:37:39 +0900

    다른 화면 추가 - UI작업 진행

--- app/src/main/java/com/example/intentpractice/MainActivity.java
+++ app/src/main/java/com/example/intentpractice/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.intentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button moveToOtherBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        moveToOtherBtn = findViewById(R.id.moveToOtherBtn);
+        moveToOtherBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, OtherActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
