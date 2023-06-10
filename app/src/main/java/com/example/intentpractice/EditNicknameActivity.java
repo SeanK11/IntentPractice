b7037af849aa537daa9a9f26338d78dc6e6a1986
Sunghyun Kim <wkddls423@gmail.com>
2023-06-10 21:22:05 +0900

    닉네임 변경화면 -> 입력 후 완료 클릭 시 데이터 들고 돌아가기 - 돌아간 메인화면에서 데이터 반영 별도 작업 필요

--- app/src/main/java/com/example/intentpractice/EditNicknameActivity.java
+++ app/src/main/java/com/example/intentpractice/EditNicknameActivity.java
@@ -2,12 +2,15 @@ package com.example.intentpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class EditNicknameActivity extends AppCompatActivity {
     private Button okBtn;
+    private EditText newNicknameEdt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,7 +20,13 @@ public class EditNicknameActivity extends AppCompatActivity {
         okBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                newNicknameEdt = findViewById(R.id.newNicknameEdt);
+                String inputNewNickname = newNicknameEdt.getText().toString();
 
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("nickname", inputNewNickname);
+                setResult(RESULT_OK, resultIntent);
+                finish();
             }
         });
     }
