fc2577c03fc11d5ce34b105c473993dfeb105085
Sunghyun Kim <wkddls423@gmail.com>
2023-06-10 13:48:28 +0900

    메세지를 보내는 화면/ 받는 화면 추가 작성 완료

--- app/src/main/java/com/example/intentpractice/MainActivity.java
+++ app/src/main/java/com/example/intentpractice/MainActivity.java
@@ -6,15 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private Button moveToOtherBtn;
+    private Button sendMessageBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         moveToOtherBtn = findViewById(R.id.moveToOtherBtn);
+        sendMessageBtn = findViewById(R.id.sendMessageBtn);
+
+        sendMessageBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, MessageActivity.class);
+                EditText editText = (EditText)findViewById(R.id.messageEdt);
+                String inputMessage = editText.getText().toString();
+                myIntent.putExtra("message", inputMessage);
+
+                startActivity(myIntent);
+            }
+        });
         moveToOtherBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/intentpractice/MessageActivity.java
+++ app/src/main/java/com/example/intentpractice/MessageActivity.java
@@ -3,6 +3,7 @@ package com.example.intentpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MessageActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
+
+        // 이 화면에 들어올 때 첨부된 데이터를 텍스트뷰에 반영
+        String inputMessage = getIntent().getStringExtra("message");
+
+        TextView receivedMessageTxt = findViewById(R.id.receivedMessageTxt);
+        receivedMessageTxt.setText(inputMessage);
     }
 }
